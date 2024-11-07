921eb199aededb8e38e8f02b958ab69ad5157914
mih <keniamsoaress@gmail.com>
2024-11-07 15:55:27 -0300

    incluir-validar-user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
 
 constructor(private readonly jwtService: JwtService, private readonly userService: UserService) {}
+
+async validateUser(email: string, password: string) {
+  const user = await this.userService.getUserByEmail(email)
+  
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const { password: _, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+    
+}
 
 async login(user: any) {
     const payload: JwtPayload = {
